ace9d4fb1abb00932b9ec721c0f627a7d562499c
azharfn <azharfzn12@gmail.com>
2023-06-24 12:40:54 +0700

    Publish Program

---
+++ main.js
@@ -0,0 +1,19 @@
+function profitMonitor(todayProfit, lastProfit) {
+
+  if(todayProfit > lastProfit){
+    return `Profit naik sebanyak ${todayProfit-lastProfit} point`
+    }
+  else if(todayProfit < lastProfit){
+    return `Profit turun sebanyak ${Math.abs(todayProfit-lastProfit)} point`
+    }
+  else{
+    return `Profit stabil`
+    }
+
+}
+
+console.log(profitMonitor(100, 50));
+console.log(profitMonitor(50, 100));
+console.log(profitMonitor(100, 100));
+
+module.exports = profitMonitor;
